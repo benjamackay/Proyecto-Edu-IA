36e0f6cd905be561af91b56eb8ad1714421e0256
benja mackay <benja.amackay@gmail.com>
2024-11-27 12:55:58 -0300

    FIX texto servicios

--- src/components/Servicios/InformacionServiciosComponent.vue
+++ src/components/Servicios/InformacionServiciosComponent.vue
@@ -11,7 +11,7 @@
   </div>
 
       <!-- Contenedor de las tarjetas -->
-      <div class="flex flex-col md:w-[100%] md:pt-8 md:pb-8 ">
+      <div class="flex flex-col md:w-[100%] ml-6 md:ml-0 md:pt-8 md:pb-8 ">
         <!-- Tarjeta 1 -->
         <div
           class="flex flex-col md:flex-row bg-white w-[90%] h-auto md:h-48 m-4 rounded-lg hover:scale-105 shadow-xl duration-300 ease-out items-center"
@@ -25,11 +25,10 @@
           </div>
           <div class="flex flex-col items-center justify-center text-center p-8">
             <p class="text-black text-xl md:text-2xl mb-4 font-mono font-bold">
-              PREDICCIONES Y RECOMENDACIONES
+              LOREM IMPSUM LOREM IPSUM
             </p>
             <p class="text-black text-base md:text-lg">
-              Con machine learning, AgroPred ofrece predicciones y
-              recomendaciones personalizadas.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum illo voluptatibus quisquam cumque, incidunt assumenda amet aperiam repudiandae autem at, quae enim vel alias neque pariatur! Corrupti culpa dolor minima?
             </p>
           </div>
         </div>
@@ -46,10 +45,9 @@
             />
           </div>
           <div class="flex flex-col items-center justify-center text-center p-8">
-            <p class="text-black text-xl md:text-2xl mb-4 font-mono font-bold">GESTIÓN Y TRAZABILIDAD</p>
+            <p class="text-black text-xl md:text-2xl mb-4 font-mono font-bold">LOREM IPSUM LOREM IPSUM</p>
             <p class="text-black text-base md:text-lg">
-              Lleva un seguimiento detallado de cada etapa del proceso agrícola,
-              desde la siembra hasta la cosecha.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem laudantium molestiae magnam voluptates, facere harum quis numquam veniam eligendi ratione est saepe ipsum voluptas nulla quam repudiandae sint commodi quisquam.
             </p>
           </div>
         </div>
@@ -66,13 +64,31 @@
             />
           </div>
           <div class="flex flex-col items-center justify-center text-center p-8">
-            <p class="text-black text-xl md:text-2xl mb-4 font-mono font-bold">PLATAFORMA INTEGRAL</p>
+            <p class="text-black text-xl md:text-2xl mb-4 font-mono font-bold">LOREM IPSUM LOREM IPSUM</p>
             <p class="text-black text-base md:text-lg">
-              Centraliza toda la información en un panel intuitivo, permitiendo
-              una visión completa.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat dolores consequuntur voluptate rem praesentium, animi suscipit fuga cum molestias! Doloribus unde saepe, expedita ea ipsum nesciunt placeat eaque maiores a.
             </p>
           </div>
         </div>
+
+        <div
+          class="flex flex-col md:flex-row bg-white w-[90%]  h-auto md:h-48 m-4 rounded-lg hover:scale-105 shadow-xl duration-300 ease-out items-center"
+        >
+          <div class="flex-shrink-0 p-4">
+            <img
+              src="@/assets/search.png"
+              alt="Google"
+              class="h-16 w-16 object-contain"
+            />
+          </div>
+          <div class="flex flex-col items-center justify-center text-center p-8">
+            <p class="text-black text-xl md:text-2xl mb-4 font-mono font-bold">LOREM IPSUM LOREM IPSUM</p>
+            <p class="text-black text-base md:text-lg">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat dolores consequuntur voluptate rem praesentium, animi suscipit fuga cum molestias! Doloribus unde saepe, expedita ea ipsum nesciunt placeat eaque maiores a.
+            </p>
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
